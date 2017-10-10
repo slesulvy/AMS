90fb9e18424e35844b791afeaa7c8b76df4bd2d1
Ulvy <slehulvy@gmail.com>
2017-10-10 14:55:22 +0700

    change something in vue

---
+++ front-end/attendance/src/components/dashboard/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="container">Dashboard</h1>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: 'Welcome to Dashboard'
+    }
+  }
+}
+</script>
--- front-end/attendance/src/router/index.js
+++ front-end/attendance/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import index from '@/components/dashboard/index'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/dashboard/index',
+      name: 'dashboard',
+      component: index
     }
   ]
 })

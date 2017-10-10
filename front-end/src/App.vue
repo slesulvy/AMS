23ac2eac97c404d4c4abcdccc9e90dfd680d092a
Chanthou <chanthou_choub@hotmail.com>
2017-10-10 16:34:42 +0700

    Update front Vue js

---
+++ front-end/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+   <nav class="navbar navbar-light navbar-toggleable-md" style="background-color: #e3f2fd;">
+      <div class="container">
+          <!-- Navbar content -->
+          <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            <img src="./assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            <span class="text-uppercase">Attendance System</span>
+          </a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <router-link class="nav-link" to="/">Home <span class="sr-only">(current)</span></router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/">Attendance</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/signin">Sign In</router-link>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  User Profile
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+      </div>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+</style>
---
+++ front-end/src/components/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="test">
+     <div class="container">
+        <button class="btn btn-danger">Go</button>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      email: 'Hello',
+      password: 'World'
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
